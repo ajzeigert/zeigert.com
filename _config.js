2a3804034b7c50358262754ee767000ca830919a
Andy Zeigert <ajzeigert@gmail.com>
2023-12-13 09:50:24 -0800

    updates plugins, renames assets to img

--- _config.js
+++ _config.js
@@ -1,29 +1,10 @@
 import lume from "lume/mod.ts";
-import unocss from "lume/plugins/unocss.ts";
-// import pageFind from "lume/plugins/pagefind.ts";
-import codeHighlight from "lume/plugins/code_highlight.ts";
-import sitemap from "lume/plugins/sitemap.ts";
-import favicon from "lume/plugins/favicon.ts";
-import nav from "lume/plugins/nav.ts";
-// import feed from "lume/plugins/feed.ts";
+import plugins from "./plugins.ts";
 
 const site = lume({
-    src: "src",
+  src: "./src",
 });
 
-site.copy("assets")
-    .use(codeHighlight())
-    .use(sitemap())
-    .use(favicon())
-    .use(nav())
-    // .use(pageFind({
-    //     ui: {
-    //         resetStyles: false,
-    //     }
-    // }))
-    // .use(feed())
-    .use(unocss({
-        // reset: 'eric-meyer'
-    }))
+site.use(plugins());
 
 export default site;
--- src/posts/2023-12-09_helloworld.md
+++
@@ -1,8 +0,0 @@
----
-title: Hello, world!
-description: And another thing
-date: 2023-12-09
-tags: 
----
-
-This is a blog post
