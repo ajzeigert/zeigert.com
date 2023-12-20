89d76cb37b15b9583f782cb40aeee3f1c1b248cd
Andy Zeigert <ajzeigert@gmail.com>
2023-12-20 12:49:31 -0800

    updates templates, adds first post

--- _config.js
+++ _config.js
@@ -1,15 +1,15 @@
 import lume from "lume/mod.ts";
 import blog from "https://deno.land/x/lume_theme_simple_blog/mod.ts";
-// import favicon from "lume/plugins/favicon.ts";
-// import picture from "lume/plugins/picture.ts";
-// import transformImages from "lume/plugins/transform_images.ts";
+import favicon from "lume/plugins/favicon.ts";
+import picture from "lume/plugins/picture.ts";
+import transformImages from "lume/plugins/transform_images.ts";
 
 const site = lume({
     src: "./src",
   })
   .use(blog())
-  // .use(favicon())
-  // .use(picture())
-  // .use(transformImages())
+  .use(favicon())
+  .use(picture())
+  .use(transformImages())
 
 export default site;
---
+++ src/_archetypes/post.js
@@ -0,0 +1,12 @@
+export default function(title) {
+    const slug = title.replace(/\s+/g, "-").toLowerCase();
+    return {
+        path: `posts/${slug}.md`,
+        content: {
+            title,
+            date: new Date(),
+            draft: true,
+            content: "Post content",
+        }
+    }
+}
